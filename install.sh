8394f54f192fc67cd1f0e40b0758cc6158ff8906
zarquon-42 <zarquon@shupe.ca>
2024-09-20 18:06:49 -0700

    Improve layout and reduce verbosity

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # Download generate scripts and set permissions
-curl --silent https://raw.githubusercontent.com/zarquon-42/knulli-led-per-game/refs/heads/main/generate_game_colour.py --output /opt/generate_game_colour.py
-curl --silent https://raw.githubusercontent.com/zarquon-42/knulli-led-per-game/refs/heads/main/led_game_colour.sh --output /userdata/system/scripts/led_game_colour.sh
-chmod +x /opt/generate_game_colour.py
-chmod +x /userdata/system/scripts/led_game_colour.sh
-
-# Save the changes
-batocera-save-overlay
+echo "Downloading Scripts..."
+curl \
+    --silent \
+    --location \
+    https://raw.githubusercontent.com/zarquon-42/knulli-led-per-game/refs/heads/main/generate_game_colour.py \
+    --output /opt/generate_game_colour.py && \
+curl \
+    --silent \
+    https://raw.githubusercontent.com/zarquon-42/knulli-led-per-game/refs/heads/main/led_game_colour.sh \
+    --output /userdata/system/scripts/led_game_colour.sh && \
+echo "Setting Permissions..." && \
+chmod +x /opt/generate_game_colour.py && \
+chmod +x /userdata/system/scripts/led_game_colour.sh && \
+echo "Making changes permanent..." && \
+batocera-save-overlay >/dev/null && \
+echo Done
